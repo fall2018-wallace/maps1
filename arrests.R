d84ae83a036a6e554a592c3468afa491a0410a65
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:28:23 -0400

    Saving arrests.R

--- arrests.R
+++ arrests.R
@@ -1,3 +1,10 @@
 
 arrests <- data.frame(USArrests)
 View(arrests)
+
+#3) Create a merged dataframe -- with the attributes from both dataframes
+arrests$stateName <- row.names(arrests) #adding the name of the column 
+mergedf <- merge(arrests,states3,by="stateName")
+View(mergedf)
+
+
